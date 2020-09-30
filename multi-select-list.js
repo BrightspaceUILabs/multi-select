84ca3518cca98a75aa154c8f0c2da251be0b80ec
Marc Soiferman <Marc.Soiferman@D2L.com>
2020-09-30 16:42:47 -0500

    Maybe it doesn't like 'unset'?

--- multi-select-list.js
+++ multi-select-list.js
@@ -315,7 +315,7 @@ class D2LMultiSelectList extends mixinBehaviors(
 		const container = this.shadowRoot.querySelector('.list-item-container');
 
 		if (this.shrinkwrap) {
-			// container.style.maxWidth = 'unset';
+			container.style.maxWidth = 'none';
 		}
 
 		let childrenWidthTotal = 0;
