0c39328640fbfe251d8e0820f9761551bd0ba6d2
Bradley Hamelin <Bradley.Hamelin@D2L.com>
2021-11-26 12:16:47 -0500

    consistency

--- lang/cy-gb.js
+++ lang/cy-gb.js
@@ -3,6 +3,6 @@
 export default {
 	hide: "Cuddio",
 	hiddenChildren: "+ {num} arall",
-	picker_remove_value: "Cliciwch i dynnu’r gwerth {value}",
+	picker_remove_value: "Cliciwch i dynnu'r gwerth {value}",
 	picker_add_value: "Cliciwch i ychwanegu'r gwerth {value}"
 };
