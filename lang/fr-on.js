9b3f9baeadb39cf0fc65466b9f1b92939b095609
Bradley Hamelin <55998273+BhamelinD2L@users.noreply.github.com>
2022-01-13 16:42:04 -0500

    fix: fix var changes

--- lang/fr-on.js
+++ lang/fr-on.js
@@ -3,6 +3,6 @@
 export default {
 	hide: "Masquer",
 	hiddenChildren: "+ {num} plus",
-	picker_remove_value: "Cliquer pour supprimer la valeur {valeur}",
-	picker_add_value: "Cliquer pour ajouter la valeur {valeur}"
+	picker_remove_value: "Cliquer pour supprimer la valeur {value}",
+	picker_add_value: "Cliquer pour ajouter la valeur {value}"
 };
