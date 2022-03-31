df174eefeb03f53dfe8353f3be2f997dca359b19
Bradley Hamelin <Bradley.Hamelin@D2L.com>
2022-03-31 11:25:36 -0400

    remove localization

--- multi-select-list-item.js
+++ multi-select-list-item.js
@@ -4,10 +4,9 @@ import '@brightspace-ui/core/components/offscreen/offscreen.js';
 import '@brightspace-ui/core/components/tooltip/tooltip.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { bodyCompactStyles } from '@brightspace-ui/core/components/typography/styles.js';
-import { Localizer } from './localization.js';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
 
-class MultiSelectListItem extends RtlMixin(Localizer(LitElement)) {
+class MultiSelectListItem extends RtlMixin(LitElement) {
 	static get properties() {
 		return {
 
@@ -235,7 +234,7 @@ class MultiSelectListItem extends RtlMixin(Localizer(LitElement)) {
 					<div class="d2l-labs-multi-select-list-item-text" aria-hidden="true">${this._getVisibleText(this.text, this.shortText, this.maxChars)}</div>
 					<d2l-offscreen>${this._getScreenReaderText(this.text, this.shortText)}</d2l-offscreen>
 				</div>
-				<d2l-icon role="button" aria-label="${this.localize('picker_remove_value', 'value', this.text)}" class="d2l-labs-multi-select-delete-icon" icon="d2l-tier1:close-large-thick" ?hidden="${!this.deletable}" @click="${this._onDeleteItem}"></d2l-icon>
+				<d2l-icon role="button" aria-label="test" class="d2l-labs-multi-select-delete-icon" icon="d2l-tier1:close-large-thick" ?hidden="${!this.deletable}" @click="${this._onDeleteItem}"></d2l-icon>
 			</div>
 			${this._hasTooltip(this.text, this.shortText, this.maxChars) ? html`
 				<d2l-tooltip position="${this.tooltipPosition}">${this.text}</d2l-tooltip>` : null }
