f360ddabdbdf2c5f2b361238aa58437c6b72911a
ProdBuildUser <ProdBuildUser@d2l.com>
2023-08-28 14:41:34 +0000

    Translation Updates

--- lang/ko.js
+++ lang/ko.js
@@ -4,7 +4,7 @@ export default {
 	delete: "삭제",
 	hide: "숨기기",
 	hiddenChildren: "{num}개 이상",
-	oneAttributeRequired: "At least one value must be set", // Tooltip that appears when no values have been set and we need to inform the user that this is an invalid state
+	oneAttributeRequired: "하나 이상의 값을 설정해야 합니다.", // Tooltip that appears when no values have been set and we need to inform the user that this is an invalid state
 	picker_remove_value: "클릭하여 {value} 값을 제거합니다.",
 	picker_add_value: "클릭하여 값 {value}을(를) 추가합니다."
 };
