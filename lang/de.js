fd9289223f37ebccff51e0fb38456a1e4aae97bb
ChrisLabattD2L <chris.labatt@d2l.com>
2023-08-29 08:38:18 -0400

    Applied DE change

--- lang/de.js
+++ lang/de.js
@@ -4,7 +4,7 @@ export default {
 	delete: "Löschen",
 	hide: "Ausblenden",
 	hiddenChildren: "+ {num} weitere",
-	oneAttributeRequired: "Mindestens eine Gewichtung muss [0] betragen.", // Tooltip that appears when no values have been set and we need to inform the user that this is an invalid state
+	oneAttributeRequired: "Mindestens ein Wert muss festgelegt werden", // Tooltip that appears when no values have been set and we need to inform the user that this is an invalid state
 	picker_remove_value: "Klicken Sie, um den Wert {value} zu entfernen",
 	picker_add_value: "Klicken Sie, um den Wert {value} hinzuzufügen"
 };
